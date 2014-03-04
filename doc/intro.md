cb2632ff3c52f6e14d81ed6349ac374cf7982f3e
Zach Markin <zachmarkin@gmail.com>
2014-03-04 14:19:46 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to img-process
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ src/img_process/core.clj
@@ -0,0 +1,36 @@
+(ns img-process.core
+  (:import (java.io BufferedReader FileReader)))
+
+(defn process-file [file-name line-func line-acc]
+  (with-open [rdr (BufferedReader. (FileReader. file-name))]
+    (reduce line-func line-acc (line-seq rdr))))
+
+(defn process-line [acc line]
+  (+ acc 1))
+
+(prn (process-file "resources/img-data.txt" process-line 1))
+
+(with-open [rdr (clojure.java.io/reader "resources/data2.txt")]
+  (println (take 3 (line-seq rdr)))
+  (println (take 2 (line-seq rdr))))
+
+(def sample "'4': 251,
+     '5': 251,
+     '6': 251,
+     '7': 255,")
+
+;;clojure.string/split
+
+
+(defn clean-text [pix-text] (clojure.string/split (purge-chars pix-text) #"\n"))
+
+(defn purge-chars [dirty-text] (clojure.string/replace
+                                 (clojure.string/replace
+                                   (clojure.string/replace dirty-text
+                                                           #"['\s]" "")
+                                  #"[,]" "\n")
+                                #"[:]" " "))
+
+(def sample1 (clean-text sample))
+sample1
+
