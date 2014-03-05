fe1c2ae85eb6792603e06bfa8ba4a6b4a6996dbd
Zach Markin <zachmarkin@gmail.com>
2014-03-05 14:36:32 -0500

    takes vector for px binary function

--- src/img_process/core.clj
+++ src/img_process/core.clj
@@ -6,11 +6,12 @@
                                    (last)
                                    (Integer/parseInt)))
 
-(defn px-val [red green blue]
-  (let [red (get-val red)
-        green (get-val green)
-        blue (get-val blue)]
-    (< (/ (+ red green blue) 3) 125)))
+(defn px-val [rgb-vec]
+  (let [rgb-vec (map get-val rgb-vec)]
+    (<
+      (/ (apply + rgb-vec)
+         3)
+      125)))
 
 (with-open [rdr (clojure.java.io/reader "resources/data2.txt")]
   (line-seq rdr)
@@ -18,4 +19,4 @@
   (line-seq rdr)
   (line-seq rdr)
   (doseq [l (partition 4 (line-seq rdr))]
-      (println (px-val (first l) (second l) (second (next l))))))
+      (println (px-val (take 3 l)))))
