10e2d42ac6b48ceea5463e1082b0e5d0f2da3e0e
Zach Markin <zachmarkin@gmail.com>
2014-03-08 22:14:23 -0500

    functioning in a non-lazy way so only functioning with small test file

--- src/img_process/core.clj
+++ src/img_process/core.clj
@@ -7,17 +7,20 @@
                                    (Integer/parseInt)))
 
 (defn px-val [rgb-vec]
-  (let [rgb-vec (map get-val rgb-vec)]
     (<
       (/ (apply + rgb-vec)
          3)
-      125)))
+      125))
 
-(with-open [rdr (clojure.java.io/reader "resources/data2.txt")]
-  (line-seq rdr)
-  (line-seq rdr)
-  (line-seq rdr)
-  (line-seq rdr)
-  (doseq [l (partition 4 (line-seq rdr))]
-      (println (px-val (take 3 l)))))
+(def value-strings (drop 3 (line-seq (clojure.java.io/reader "resources/data2.txt"))))
 
+(def value-ints (map get-val value-strings))
+
+value-ints
+
+(loop [px-bin [], ind 0, values value-ints]
+  (if (<= (count values) (* 4 (count px-bin)))
+    px-bin
+    (recur (conj px-bin (px-val (vec (butlast (take 4 (drop (* 4 ind) values))))))
+           (inc ind)
+           values)))
