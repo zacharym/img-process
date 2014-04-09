df32b6ff40d7ae6efe65b77e6c8cca60d7ac055f
Zach Markin <zachmarkin@gmail.com>
2014-04-09 00:27:00 -0400

    properly reads inputs

--- src/img_process/neural.clj
+++ src/img_process/neural.clj
@@ -24,7 +24,6 @@
           (for [i (range (count vec0))]
             (* (vec0 i) (vec1 i)))))
 
-
 (defn sig-out [in-vec weight-vec bias-vec]
   "takes a vector of inputs, a vector of neurons defined by the bias-vector, and corresponding 2-D
   vector of weights for the connections between those two. Returns the output of a layer of sigmoids
@@ -32,6 +31,8 @@
     (vec (map #(sigmoid (- (dot (weight-vec %) in-vec) (bias-vec %))) (range (count bias-vec)))))
 
 (defn vec-length [vec1]
+  "returns the length of a vector in multiple dimensions (each element represents a length in a different
+  dimension. Interestingly the pythagorean theorem is the 2 dimensional case of this."
   (->> vec1
    (map #(math/expt % 2))
    (reduce +)
@@ -51,38 +52,43 @@
   output vector"
   (sig-out (sig-out inputs weights-a biases-a) weights-b biases-b))
 
-(defn datum-cost [inputs value weights-a biases-a weights-b biases-b]
-  (get-cost (evaluate inputs weights-a biases-a weights-b biases-b) value))
+(defn json-2-training-pair [json-vec]
+  "takes vector created by json/read-str and converts in to a key value mapping of the input
+  data and correct result"
+  {:input (json-vec 0) :result (json-vec 1)})
 
 (defn get-random-set [samp-n pop-n]
-  "returns the line numbers of the random sample set"
-  (set (sort (take samp-n (repeatedly #(rand-int pop-n))))))
+  "returns a samp-n long sequence of random integers in the range [0,pop-n).
+  No repeat values in return sequence"
+  (loop [res (list)]
+    (if (>= (count res) samp-n)
+      res
+      (let [temp (rand-int pop-n)]
+        (if (some #{temp} res)
+          (recur res)
+          (recur (conj res temp)))))))
 
-(defn get-set-values [indices file max-index]
-  "returns a collection of lines with maximum max-index for the given indices of the file"
-   (with-open [rdr (clojure.java.io/reader file)]
-     (let [lines (line-seq rdr)]
-       (loop [data [] ct 0]
-         (if (> ct (+ 1 max-index))
-           data
-           (if (not (empty? (filter #(= ct %) indices))) ;;if the ct is one of the selected indices
-             (recur
-              (conj data (last (take ct lines)))
-              (inc ct))
-           (do
-             (recur
-             data
-             (inc ct)))))))))
-
-(defn json-2-KV [json-str]
-  "converts a json string of an input and correct output to a KV mapping of :input and :result as keys"
-  (let [datum (json/read-str json-str)]
-    {:input (datum 0) :result (datum 1)}))
-
-(defn extract-set-values [train-strings]
-  "takes a vector of strings as JSON representing training inputs and correct results,
-  returns them as a vector of collections with :input and :result as keys"
-  (map json-2-KV train-strings))
+(defn get-training-data [batch-size batch-num]
+  ;;;;;;; THIS WILL BE UPDATED DEPENDING ON WHICH FILE WE ARE READING DATA FROM    ;;;;;;;;;;;;;
+  "takes the size of each training batch and the number of batches, returns a 2D vector
+  of randomly selected input and output data"
+  (let [line-numbers (get-random-set (* batch-size batch-num) 99)]
+    (with-open [rdr (clojure.java.io/reader "resources/train_hundo.txt")]
+      (let [lines (line-seq rdr)]
+        (loop [i 0 f (apply max line-numbers) res [] lines lines]
+          (if (> i f)
+            res
+            (if (some #{i} line-numbers)
+              (recur
+               (inc i)
+               f
+               (conj res (json-2-training-pair (json/read-str (first lines))))
+               (drop 1 lines))
+              (recur
+               (inc i)
+               f
+               res
+               (drop 1 lines)))))))))
 
 (defn set-cost [train-set weights-a biases-a weights-b biases-b]
   "takes a training set of key value pairs of inputs and results and returns the cost representing how
@@ -96,64 +102,45 @@
     (* 1.1 value)
     0.99))
 
-(up-ten-percent 0.98)
+(defn down-ten-percent [value]
+  "accepts a value and returns that value decreased by 10% with a minimum value of 0.01"
+  (if (< (* 0.9 value) 0.01)
+    0.01
+    (* 0.9 value)))
 
-
-(defn weights-b-change [train-set weights-a biases-a weights biases-b]
-  "takes a training set, a vector of weights for the second layer of connections, the rest of the networks
-  settings and returns an identiy matrix where true means that the partial derivative of the total cost
-  with respect to that connection's weight is negative [for a step size of 10% of the weight's current
-  value] (increase that value to decrease the total cost)"
+(defn train-network [train-data input-n layer-a-n layer-b-n]
+  "takes a 2D array of correct input/output pairs of training data - a single row to be used for each
+  generation of training, the number of inputs to the network, the number of neurons in the first and
+  second layers. Returns a key value mapping of `trained` weights and biases for the network. W
+  orks by randomly initializing weights and biases and then following a gradient decent algorithm to
+  minimize the cost of the network over each generation w.r.t. each weight and bias."
   )
 
 
-(defn weights-a-change [train-set weights biases-a weights-b biases-b]
-  "takes a training set, a vector of weights for the first layer of connections, the rest of the network's
-  settings and returns an identiy matrix where true means that the partial derivative of the total cost
-  with respect to that connection's weight is negative [for a step size of 10% of the weight's current
-  value] (increase that value to decrease the total cost)"
-  (loop [res [] outer-ct 0]
-    (if (>= outer-ct (count weights))
-      res
-      (do
-        (loop [res-in [] inner-ct 0]
-          (if (< inner-ct (count (weights outer-ct)))
-            (let [mutant (assoc weights outer-ct (update-in (weights outer-ct) inner-ct up-ten-percent))]
-              (if (>=
-                   (set-cost train-set mutant biases-a weights-b biases-b)
-                   (set-cost train-set weights biases-a weights-b biases-b)) ;;partial derivative is positive
-                (recur
-                 (assoc res-in 0)
-                 (inc inner-ct))
-               (recur
-                (assoc res-in 1)
-                (inc inner-ct))))
-          (assoc res res-in)))
-      (recur
-       res
-       (inc outer-ct))))))
+
+(def a (get-training-data 10 5))
+
+
 
 ((init-weights-a 1) 1)
 (count (init-weights-a 0))
-(weights-a-change test-value-inputs init-weights-a init-biases-a init-weights-b init-biases-b)
 
+
+(time (def sample-result (weights-a-change test-value-inputs init-weights-a init-biases-a init-weights-b init-biases-b)))
+(def sample-result2 (biases-a-change test-value-inputs init-weights-a init-biases-a init-weights-b init-biases-b))
+sample-result2
+(def sample-result)
 
 
 init-biases-a
 (def init-biases-aa [0.56 0.4321580631740959 0.57710040224414 0.6185965659086016 0.18969736398984938 0.8749276162565557 0.7980151147402715 0.664393999821422 0.9569106331833737 0.9815324868138454 0.3432571362384952 0.6501960631145083 0.3426291107100178 0.36863149128690365 0.7569718521150473])
 
-(def indices '(3 1 2 9 43 8))
-indices
-(def test-values (get-set-values indices "resources/train_hundo.txt" (apply max indices)))
-(def test-value-inputs (extract-set-values test-values))
-test-values
-test-value-inputs
+
 (map #(:result %) test-value-inputs)
 (count test-values)
 
 (set-cost test-value-inputs init-weights-a init-biases-a init-weights-b init-biases-b)
-(set-cost test-value-inputs init-weights-a init-biases-aa init-weights-b init-biases-b)
-(set-cost1 test-value-inputs init-weights-a init-biases-a init-weights-b init-biases-b)
+(set-cost1 test-value-inputs init-weights-a in1it-biases-a init-weights-b init-biases-b)
 (set-cost2 test-value-inputs init-weights-a init-biases-a init-weights-b init-biases-b)
 test-value-inputs
 
@@ -178,3 +165,95 @@ test-value-inputs
 
 
 
+
+
+
+
+
+
+
+
+
+(defn biases-a-change [train-set weights-a biases weights-b biases-b]
+  "takes a training set, a vector of biases for the first layer of connections, the rest of the network
+  settings and returns an identity matrix where true means that the partial derivative of the total cost
+  with respect to that neuron's bias is negative [for a step size of 10% of the current bias
+  value] (increase that value to decrease the total cost)"
+  (loop [res [] ct 0]
+    (if (>= ct (count biases))
+      res
+     (recur
+      (let [mutant (assoc biases ct (up-ten-percent (biases ct)))]
+        (if (>=
+             (set-cost train-set weights-a mutant weights-b biases-b)
+             (set-cost train-set weights-a biases weights-b biases-b))
+           (conj res (down-ten-percent (biases ct)))
+          (conj res (up-ten-percent (biases ct)))))
+      (inc ct)))))
+
+(defn biases-b-change [train-set weights-a biases-a weights-b biases]
+  "takes a training set, a vector of biases for the second layer of connections, the rest of the network
+  settings and returns an identity matrix where true means that the partial derivative of the total cost
+  with respect to that neuron's bias is negative [for a step size of 10% of the current bias
+  value] (increase that value to decrease the total cost)"
+  (loop [res [] ct 0]
+    (if (>= ct (count biases))
+      res
+     (recur
+      (let [mutant (assoc biases ct (up-ten-percent (biases ct)))]
+        (if (>=
+             (set-cost train-set weights-a biases-a weights-b mutant)
+             (set-cost train-set weights-a biases-a weights-b biases))
+           (conj res (down-ten-percent (biases ct)))
+          (conj res (up-ten-percent (biases ct)))))
+      (inc ct)))))
+
+(defn weights-a-change [train-set weights biases-a weights-b biases-b]
+  "takes a training set, a vector of weights for the first layer of connections, the rest of the network's
+  settings and returns an identiy matrix where true means that the partial derivative of the total cost
+  with respect to that connection's weight is negative [for a step size of 10% of the weight's current
+  value] (increase that value to decrease the total cost)"
+  (loop [res [] outer-ct 0]
+    (if (>= outer-ct (count weights))
+      res
+     (recur
+      (conj res
+       (loop [res-in [] inner-ct 0]
+         (if (>= inner-ct (count (weights outer-ct)))
+           res-in
+          (let [mutant (assoc weights outer-ct (assoc (weights outer-ct) inner-ct (up-ten-percent ((weights outer-ct) inner-ct))))]
+            (if (>=
+                  (set-cost train-set mutant biases-a weights-b biases-b)
+                  (set-cost train-set weights biases-a weights-b biases-b)) ;;partial derivative is positive
+                (recur
+                 (conj res-in (down-ten-percent ((weights outer-ct) inner-ct)))
+                 (inc inner-ct))
+               (recur
+                (conj res-in (up-ten-percent ((weights outer-ct) inner-ct)))
+                (inc inner-ct)))))))
+      (inc outer-ct)))))
+
+(defn weights-b-change [train-set weights-a biases-a weights biases-b]
+  "takes a training set, a vector of weights for the second layer of connections, the rest of the networks
+  settings and returns an identiy matrix where true means that the partial derivative of the total cost
+  with respect to that connection's weight is negative [for a step size of 10% of the weight's current
+  value] (increase that value to decrease the total cost)"
+    (loop [res [] outer-ct 0]
+    (if (>= outer-ct (count weights))
+      res
+     (recur
+      (conj res
+       (loop [res-in [] inner-ct 0]
+         (if (>= inner-ct (count (weights outer-ct)))
+           res-in
+          (let [mutant (assoc weights outer-ct (assoc (weights outer-ct) inner-ct (up-ten-percent ((weights outer-ct) inner-ct))))]
+            (if (>=
+                  (set-cost train-set weights-a biases-a mutant biases-b)
+                  (set-cost train-set weights-a biases-a weights biases-b)) ;;partial derivative is positive
+                (recur
+                 (conj res-in (down-ten-percent ((weights outer-ct) inner-ct)))
+                 (inc inner-ct))
+               (recur
+                (conj res-in (up-ten-percent ((weights outer-ct) inner-ct)))
+                (inc inner-ct)))))))
+      (inc outer-ct)))))
